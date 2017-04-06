b1224327cea0db9407340ec6d20433fd0e9731d7
Rachel Ambler <RAmbler@PureRomance.com>
2017-04-06 06:24:53 -0400

    Added missing file from Codeplex

---
+++ SS-eye-S/Top/IContainer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SqlServer.Dts.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomThunks.API.SSiS
+{
+    public interface IContainer
+    {
+        iVariable AddVariable(string name, string description, string nameSpace, object value);
+        iVariable AddVariable(string name, string description, object value);
+        iVariable AddExpression(string name, string description, string expression);
+        iVariable AddExpression(string name, string description, string nameSpace, string expression);
+
+        T AddTask<T>(string name, string description) where T : iControlFlowTask, new();
+
+        iControlFlowTask AddTask(string iComponentName, string name, string description);
+
+        iVariable GetVariable(string name);
+
+       iVariable GetVariable(string name, string nameSpace);
+    }
+}
